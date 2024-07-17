3f8fd0434c17d876d2c16b830b2d082490164d6b
Yash Mittal <yashmittalmm@gmail.com>
2024-07-17 21:17:49 +0530

    responsiveness of demo modal

--- src/components/PresentationModal.tsx
+++ src/components/PresentationModal.tsx
@@ -5,19 +5,20 @@ export function PresentationModal() {
     return (
         <Dialog>
             <DialogTrigger asChild>
-                <button className="inline-block text-blue-300 no-underline hover:text-pink-500 flex items-center gap-2 hover:text-underline text-center h-10 p-2 md:h-auto md:p-4 transform hover:scale-125 duration-300 ease-in-out">
+                <button className="no-underline  flex items-center gap-2 hover:text-underline text-center h-10 p-2 md:h-auto md:p-4 transform hover:scale-125 duration-300 ease-in-out">
                     <VideoIcon />
                     <span>Watch Demo</span>
                 </button>
             </DialogTrigger>
-            <DialogContent className="sm:max-w-[425px]">
+            <DialogContent className="sm:max-w-[425px] text-black bg-transparent backdrop-blur-md border-none bg-gradient-to-r from-green-400 via-pink-500 to-purple-500">
                 <div className="p-4">
                     <video
                         src="/video/demo.mov"
-                        width={600}
-                        height={300}
+                        width={400}
+                        height={500}
                         controls={true}
                         muted={true}
+                        className="max-h-[calc(100vh-115px)]"
                     />
                 </div>
             </DialogContent>
