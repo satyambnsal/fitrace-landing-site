a6dcb4920b6b38c85716d7dd7b93050d0efd92f1
Yash Mittal <yashmittalmm@gmail.com>
2024-07-18 13:51:46 +0530

    change github url

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,7 @@
 export const DEV_PORT = 3000;
 export const PRODUCT_NAME = 'Fitrace';
 export const TWITTER_URL = 'https://x.com/FitRaceSocial';
-export const GITHUB_URL = 'https://github.com/satyambnsal/fitrace-contracts';
+export const GITHUB_URL = 'https://github.com/Fitrace/fitrace-contracts';
 export const DOCS_URL =
   'https://docs.google.com/document/d/1T3x5DbrYpajoRG8OCwedjmGyo_4U4fRl3cbVcuTZEEc/edit?usp=sharing';
 export const PRESENTATION_URL = 'https://pitch.com/v/fitrace-wf8hmn';
