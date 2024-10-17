d4df461f84ccf3f84009573641d72eb87fc388b0
ankit.agrawal <ankit.agrawal@inmobi.com>
2024-10-17 20:45:56 +0530

    add for account deployment

---
+++ src/app/starknet/page.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import { MyStarkApp } from '@/components/MyStarkApp';
+
+export default function Page() {
+    return (
+        <div>
+            <MyStarkApp />
+        </div>
+    );
+}
---
+++ src/components/MyStarkApp.tsx
@@ -0,0 +1,7 @@
+export const MyStarkApp = () => {
+    return (
+        <div>
+            <h1 className="bg-white">My Stark App</h1>
+        </div>
+    );
+};
